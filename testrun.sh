58d3d9304583276060582f413b24ac53e95d8ebf
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-10-11 18:46:07 +0300

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -11,6 +11,5 @@ sed -i 's/userpass/135790ecomap/g' /home/ubuntu/home/workspace/test_ecomap/ecoma
 sed -i 's/dbname/ecomap_db/g' /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_configvars.conf
 sed -i 's/dbname/ecomap_db/g' /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/db.conf
 sed -i 's/dbname/ecomap_db/g' /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/dump.sh
-pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
 sed -i "s/ecomapIP/$(echo "$(hostname -I)" | sed 's/^2.//;s/.$//')/g" /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf
-
+pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
