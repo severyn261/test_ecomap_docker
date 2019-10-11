e5a8ef8bca0e5d963bf2cbcfaf274832d9098db2
sevko <sevko@hpa.pot>
2019-10-11 16:44:54 +0300

    remake

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM ubuntu
+
+RUN apt-get update && apt-get install -y \
+   libmysqlclient-dev \
+   apache2 \
+   libapache2-mod-wsgi \
+   libapache2-mod-wsgi python-dev \
+   memcached \
+   python-pip \
+   libxml2-dev \
+   libxslt-dev \
+   libssl-dev \
+   python-dev \
+   libgirepository1.0-dev \
+   libcairo2-dev \
+   libjpeg-dev \
+   libgif-dev \
+   libhashkit2 \
+   libhashkit-dev \
+   libmemcached-dev \
+   libmemcached11 \
+   libmemcachedutil2 \
+   libsasl2-dev\
+   wget\
+   unzip\
+   git \
+   libffi-dev \
+   libxslt1-dev \
+   mysql-client \
+&& apt-get clean && rm -rf /var/lib/apt/lists/*
+
+RUN adduser ubuntu; echo ubuntu:ubuntu
+RUN mkdir /home/ubuntu/home
+RUN mkdir /home/ubuntu/home/workspace
+RUN cd /home/ubuntu/home/workspace; git clone https://github.com/severyn261/test_ecomap_docker.git
+RUN chown ubuntu:ubuntu -R /home/ubuntu/home & chmod 777 -R /home/ubuntu/home
+RUN bash /home/ubuntu/home/workspace/test_ecomap_docker/testrun.sh
--- installing.sh
+++ installing.sh
@@ -1,40 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y mysql-server \
-            libmysqlclient-dev \
-            apache2 \
-            libapache2-mod-wsgi \
-            libapache2-mod-wsgi python-dev \
-            memcached \
-            python-pip \
-            libxml2-dev \
-            libxslt-dev \
-            libssl-dev \
-            python-dev \
-            libgirepository1.0-dev \
-            libcairo2-dev \
-            libjpeg-dev \
-            libgif-dev \
-            libhashkit2 \
-            libhashkit-dev \
-            libmemcached-dev \
-            libmemcached11 \
-            libmemcachedutil2 \
-            libsasl2-dev\
-            wget\
-            unzip
-echo "Packages installed" > /home/ubuntu/README.TXT
-sudo pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
-echo "pip packages installed" >> /home/ubuntu/README.TXT
-# sudo bash /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/dump.sh
-# sudo rm /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/dump.sh
-# echo "db dumped" >> /home/ubuntu/README.TXT
+pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
 sleep 10s
-sudo a2dissite 000-default
-sudo systemctl reload apache2
-sudo sed -i "s/ecomapIP/$(echo "$(hostname -I)" | sed 's/^2.//;s/.$//')/g" /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf
-sudo cp /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
-sudo a2ensite ecomap
-sudo systemctl reload apache2
+a2dissite 000-default
+systemctl reload apache2
+sed -i "s/ecomapIP/$(echo "$(hostname -I)" | sed 's/^2.//;s/.$//')/g" /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf
+cp /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
+a2ensite ecomap
+systemctl reload apache2
 echo "ALL TASKS ARE OK" > /home/ubuntu/README.TXT
